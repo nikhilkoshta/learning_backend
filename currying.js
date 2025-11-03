85ceeaecd2ae2ec36b8387ed6b1d32891b40fd32
Nikhil <nikhilkoshta.work@gmail.com>
2025-11-03 16:22:53 +0000

    new

--- currying.js
+++ currying.js
@@ -9,18 +9,22 @@ function addNumbers(a) {
 console.log(addNumbers(2)(3)(10));
 
 
-function sendAutoEmail(to) {
-    return function(subject) {
-        return function(body) {
-            console.log(`Send Email to: ${to} with the subject - ${subject}. Here's your message - ${body}`);
-        }
-    }
-}
+// function sendAutoEmail(to) {
+//     return function(subject) {
+//         return function(body) {
+//             console.log(`Send Email to: ${to} with the subject - ${subject}. Here's your message - ${body}`);
+//         }
+//     }
+// }
 
+//Now with mordern javascript code :
+
+
+const sendAutoEmail = (to) => (subject) => (body) => `Send Email to: ${to} with the subject - ${subject}. Here's your message - ${body}`;
 let step1 = sendAutoEmail("nikhilkoshta2@gmail.com");
 
 let step2 = step1("New Order Confirmation");
 
-step2(`Hello dear Nikhil Your order is confirmed`);
+console.log(step2(`Hello dear Nikhil Your order is confirmed`));
 
 
