a07cda6f6457fed3ff3a3c7822db09b0d81e9bae
Nikhil <nikhilkoshta.work@gmail.com>
2025-11-24 16:10:23 +0000

    changes

--- learning-db/mongo/index.js
+++ learning-db/mongo/index.js
@@ -5,3 +5,11 @@ const adminRouter = require("./routes/admin");
 const userRouter = require("./routes/user");
 
 app.use(bodyParser.json());
+app.use("/admin", adminRouter);
+app.use("/user", userRouter);
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+})
