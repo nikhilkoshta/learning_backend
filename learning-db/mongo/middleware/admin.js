e51665296b27363b25561b91abc25127e92eb8f5
Nikhil <nikhilkoshta.work@gmail.com>
2025-11-25 07:33:00 +0000

    add auth for admin

--- learning-db/mongo/middleware/admin.js
+++ learning-db/mongo/middleware/admin.js
@@ -1,9 +1,25 @@
 
 //Middleware for handling auth
+const { Admin } = require("../db");
 
-function adminMiddleware(req, res, next) {
+async function adminMiddleware(req, res, next) {
     //Implement admin auth logic
-    //You need to check the headers and validate admin from the admin Dashboard
+    //You need to check the headers and validate admin from the admin DB.
+    const username = req.headers.username;
+    const password = req.headers.password;
+
+    const user = await Admin.findOne({
+        username: username,
+        password: password
+    });
+
+    if (user) {
+        next();
+    } else {
+        res.status(403).json({
+            msg: "User doesn't exist"
+        })
+    }
 }
 
 module.exports = adminMiddleware;
