39f061867a998c0dfaf692bf518e25bc13b5496a
Nikhil <nikhilkoshta.work@gmail.com>
2025-11-25 07:05:20 +0000

    add userMiddleware

---
+++ learning-db/mongo/middleware/user.js
@@ -0,0 +1,7 @@
+function userMiddleware(req, res, next) {
+    //Implement auth logic
+    //Need to check the headers and validate the user from the user DB. Check reqadme for the exact headers to be expected
+
+}
+
+module.exports = userMiddleware;
