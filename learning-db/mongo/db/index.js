55b24380c7cdad43b1ab0e9da8d7a8bb488742b4
Nikhil <nikhilkoshta.work@gmail.com>
2025-11-25 07:24:10 +0000

    finished db part

--- learning-db/mongo/db/index.js
+++ learning-db/mongo/db/index.js
@@ -1,17 +1,26 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongo-url");
+mongoose.connect("mongodb+srv://kirags123:8qPEa8KTKBEh2bss@cluster0.f3qlbuo.mongodb.net/");
 
 const AdminSchema = new mongoose.Schema({
-
+    username: String,
+    password: String
 });
 
 const UserSchema = new mongoose.Schema({
-
+    username: String,
+    password: String,
+    purchasedCourses: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Course'
+    }]
 });
 
 const CourseSchema = new mongoose.Schema({
-
+    title: String,
+    description: String,
+    price: Number,
+    imageLink: String
 });
 
 const Admin = mongoose.model('Admin', AdminSchema);
