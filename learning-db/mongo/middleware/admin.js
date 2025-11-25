179069ce1724c82f2076fbc19c6230aa0dba4531
Nikhil <nikhilkoshta.work@gmail.com>
2025-11-25 06:54:11 +0000

    add adminMiddleware

---
+++ learning-db/mongo/middleware/admin.js
@@ -0,0 +1,9 @@
+
+//Middleware for handling auth
+
+function adminMiddleware(req, res, next) {
+    //Implement admin auth logic
+    //You need to check the headers and validate admin from the admin Dashboard
+}
+
+module.exports = adminMiddleware;
