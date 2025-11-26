cbda1e9fd7731c14bc78b8daa9153f86c51505d0
Nikhil <nikhilkoshta.work@gmail.com>
2025-11-26 06:47:39 +0000

    completed /signup route

--- learning-db/mongo/routes/admin.js
+++ learning-db/mongo/routes/admin.js
@@ -1,12 +1,22 @@
 const { Router } = require("express");
 const adminMiddleware = require("../middleware/admin");
+const { Admin } = require("../db");
 const router = Router();
 
 //Admin Routes
 
-router.post('/signup', (req, res) => {
+router.post('/signup', async (req, res) => {
     const username = req.body.username;
     const password = req.body.password;
+
+    await Admin.create({
+        username,
+        password
+    })
+
+    res.json({
+        msg: "Admin created successfully"
+    })
 });
 
 router.post('/courses', adminMiddleware, (req, res) => {
