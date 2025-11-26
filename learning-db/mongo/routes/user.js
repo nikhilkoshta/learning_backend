07a61aee62bf39051ce45a992012d7394bdd78db
Nikhil <nikhilkoshta.work@gmail.com>
2025-11-26 07:32:19 +0000

    added mongoose

--- learning-db/mongo/routes/user.js
+++ learning-db/mongo/routes/user.js
@@ -1,17 +1,41 @@
 const { Router } = require("express");
 const router = Router();
 const userMiddleware = require("../middleware/user");
+const { User, Course } = require("../db");
 
-router.post('/signup', (req, res) => {
+router.post('/signup', async (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
 
+    await User.create({
+        username,
+        password
+    })
+
+    res.json({
+        msg: "User created successfully"
+    })
 })
 
-router.get('/courses', (req, res) => {
+router.get('/courses', async (req, res) => {
+    const allCourses = await Course.find({});
 
+    res.json({
+        courses: allCourses
+    })
 })
 
-router.post('/courses/:courseId', userMiddleware, (req, res) => {
+router.post('/courses/:courseId', userMiddleware, async (req, res) => {
+    const courseId = req.params.courseId;
+    const username = req.headers.username;
 
+    User.updateOne({
+        username: username
+    }, {
+        purchasedCourses: {
+            "$push": courseId
+        }
+    })
 })
 
 router.get('/purchasedCourses', userMiddleware, (req, res) => {
