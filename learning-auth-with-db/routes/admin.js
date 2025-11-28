4f8b88b9b057341f8a243bbdef6127a30f5da5ad
Nikhil <nikhilkoshta.work@gmail.com>
2025-11-28 07:40:27 +0000

    add signin route for admin

---
+++ learning-auth-with-db/routes/admin.js
@@ -0,0 +1,55 @@
+const { Router } = require("express");
+const adminMiddleware = require("../middleware/admin");
+const router = Router();
+const jwt = require("jsonwebtoken");
+const JWT_SECRET = require("..");
+
+// Admin Routes
+router.post('/signup', async (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    await Admin.create({
+        username: username,
+        password: password
+    })
+
+    res.json({
+        msg: "Admin created successfully"
+    })
+});
+
+router.post('/signin', async (req, res) => {
+    // Implement admin signup logic
+    const username = req.body.username;
+    const password = req.body.password;
+
+    const user = await User.find({
+        username,
+        password
+    });
+
+    if (user) {
+        const token = jwt.sign({
+            username
+        }, JWT_SECRET);
+
+        res.json({
+            token
+        })
+    } else {
+        res.status(411).json({
+            msg: "Wrong Inputs"
+        })
+    }
+});
+
+router.post('/courses', adminMiddleware, (req, res) => {
+    // Implement course creation logic
+});
+
+router.get('/courses', adminMiddleware, (req, res) => {
+    // Implement fetching all courses logic
+});
+
+module.exports = router;
