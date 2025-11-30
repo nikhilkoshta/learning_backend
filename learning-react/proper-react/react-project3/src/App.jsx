64a3afcca15d84185391b1cad47316f4fca4aa5f
Nikhil <nikhilkoshta.work@gmail.com>
2025-11-30 10:52:23 +0530

    Refactor counter button into CustomButton component

--- learning-react/proper-react/react-project3/src/App.jsx
+++ learning-react/proper-react/react-project3/src/App.jsx
@@ -4,14 +4,20 @@ import './App.css'
 function App() {
   const [count, setCount] = useState(0)
 
-  function onClickHandler() {
-    setCount(count + 1);
-  }
+  
   return (
     <div>
-      <button onClick={onClickHandler}>Counter {count}</button>
+      <CustomButton count={count} setCount={setCount}></CustomButton>
     </div>
   )
+}
+
+function CustomButton(props) {
+  function onClickHandler() {
+    props.setCount(props.count + 1);
+  }
+  
+  return <button onClick={onClickHandler}>Counter {props.count}</button>
 }
 
 export default App
