1ecb5f6096f630c4d5d4f65b10602101327b80b1
Nikhil <nikhilkoshta.work@gmail.com>
2025-11-30 10:46:03 +0530

    Add button to increment counter in App component

--- learning-react/proper-react/react-project3/src/App.jsx
+++ learning-react/proper-react/react-project3/src/App.jsx
@@ -4,10 +4,13 @@ import './App.css'
 function App() {
   const [count, setCount] = useState(0)
 
+  function onClickHandler() {
+    setCount(count + 1);
+  }
   return (
-    <>
-      
-    </>
+    <div>
+      <button onClick={onClickHandler}>Counter {count}</button>
+    </div>
   )
 }
 
