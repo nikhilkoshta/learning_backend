122f03bcd1a0c5db58a8d8cd731a9cac977417ed
Nikhil <nikhilkoshta.work@gmail.com>
2025-12-02 17:39:16 +0000

    added a todo app using react

--- learning-react/todo-react/todo/src/App.jsx
+++ learning-react/todo-react/todo/src/App.jsx
@@ -3,13 +3,37 @@ import './App.css'
 import CustomButton from './ui-components/CustomButton'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [todos, setTodos] = useState([{
+    title: "New Todo 1",
+    description: "Desc of new todo 1"
+  }, {
+    title: "New Todo 2",
+    description: "Desc of new todo 2"
+  }])
+
+
+  function addTodos() {
+    setTodos([...todos, {
+      title: "new Todo",
+      description: "desc of new todo"
+    }])
+  }
 
   return (
-    <>
-      <CustomButton count={count} setCount={setCount}/>
-    </>
+    <div>
+      <button onClick={addTodos}>Add Todo</button>
+      {todos.map(function(todos) {
+        return <Todo title={todos.title} description={todos.description}/>
+      })}
+    </div>
   )
+}
+
+function Todo(props) {
+  return <div>
+    <h1>{props.title}</h1>
+  <h2>{props.description}</h2>
+  </div>
 }
 
 
