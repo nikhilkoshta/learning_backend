83040f743f23a045c2c73a160548493eacec16e7
Nikhil <nikhilkoshta.work@gmail.com>
2025-12-03 12:28:22 +0000

    add backend routes

--- learning-react/new-app/backend/index.js
+++ learning-react/new-app/backend/index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { createTodo, updateTodo } = require("./types");
 const PORT = 3000;
 const app = express();
 app.use(express.json());
@@ -8,11 +9,27 @@ app.get('/todos', (req, res) => {
 })
 
 app.post('/todo', (req, res) => {
-    
+    const createPayload = req.body;
+    const parsedPayload = createTodo.safeParse(createPayload);
+
+    if (!parsedPayload.success) {
+        res.status(411).json({
+            msg: "You sent the wrong inputs"
+        })
+        return;
+    }
 })
 
 app.put('/completed', (req, res) => {
-    
+    const updatePayload = req.body;
+    const parsedPayload = updateTodo.safeParse(updatePayload);
+
+    if (!parsedPayload.success) {
+        res.status(411).json({
+            msg: "You sent the wrong inputs"
+        })
+        return;
+    }
 })
 
 app.delete('/', (req, res) => {
